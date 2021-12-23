dfa88182221e574225d1b3e9cdfdaa6bb4e7f991
Gabriel Dutra <gabrielcbdutra@gmail.com>
2021-12-23 11:23:14 -0300

    Início da implementação do projeto API

---
+++ src/main/java/com/costcontrol/CostControlJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.costcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostControlJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostControlJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/costcontrol/CostControlJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostControlJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
