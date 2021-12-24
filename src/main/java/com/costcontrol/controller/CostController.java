63f53bbf551f86cf7d93271fd363422fb9d0acfb
Gabriel Dutra <gabrielcbdutra@gmail.com>
2021-12-24 17:54:25 -0300

    Feita conexão com o banco local (Workbench)

--- src/main/java/com/costcontrol/controller/CostController.java
+++ src/main/java/com/costcontrol/controller/CostController.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import com.costcontrol.model.Custos;
 import com.costcontrol.repository.CustoRepository;
@@ -19,6 +22,12 @@ public class CostController {
 	@GetMapping
 	 public List<Custos> listarCustos() {
 		 return custoRepository.findAll();
+	 }
+	
+	@PostMapping
+	@ResponseStatus
+	 public Custos adicionar(@RequestBody Custos custo) {
+		 return custoRepository.save(custo);
 	 }
 	
 }
--- src/main/java/com/costcontrol/model/Custos.java
+++ src/main/java/com/costcontrol/model/Custos.java
@@ -1,6 +1,7 @@
 package com.costcontrol.model;
 
-import javax.annotation.Generated;
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,7 +18,42 @@ public class Custos {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(nullable = true)
+	@Column(nullable = false)
 	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Custos other = (Custos) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 
 }
