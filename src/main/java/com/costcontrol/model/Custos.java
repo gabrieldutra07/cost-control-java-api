cd50c5345d329d1c5afc65c9fa24e08aa5e3cb76
Gabriel Dutra <gabrielcbdutra@gmail.com>
2021-12-24 18:27:24 -0300

    Criação de novos atributos (model) juntamente com a modelagem do banco

--- src/main/java/com/costcontrol/model/Custos.java
+++ src/main/java/com/costcontrol/model/Custos.java
@@ -1,5 +1,6 @@
 package com.costcontrol.model;
 
+import java.sql.Date;
 import java.util.Objects;
 
 import javax.persistence.Column;
@@ -7,6 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
@@ -17,6 +21,15 @@ public class Custos {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	private String importante;
+	
+	@JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss")
+	@JsonProperty("data")
+	@Column(name = "data")
+	private Date data;
+	
+	private double valor;
 	
 	@Column(nullable = false)
 	private String descricao;
@@ -36,11 +49,37 @@ public class Custos {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	
+
+	public String getImportante() {
+		return importante;
+	}
+
+	public void setImportante(String importante) {
+		this.importante = importante;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
 
 	@Override
 	public boolean equals(Object obj) {
