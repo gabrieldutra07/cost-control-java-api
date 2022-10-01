cc15722723de96bc2d4206af58f8d5ff56b4f0ee
Gabriel Dutra <gabrielcbdutra@gmail.com>
2022-10-01 11:35:23 -0300

    .

--- src/main/java/com/costcontrol/model/Custos.java
+++ src/main/java/com/costcontrol/model/Custos.java
@@ -9,9 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.Data;
 
 @Entity
